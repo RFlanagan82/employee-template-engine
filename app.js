8ce27f73d0268522fbb1b33890ca694e15f6f5c6
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-09-27 17:47:42 -0400

    to undo the last commit since I was in the wrong folder when I completed it. This commit shows that I am troubleshooting setting up the classes for the Employee.js and Manager.js files.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -13,6 +14,9 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+console.log("Please build your team.")
+
 
 function managerPrompt() {
     return inquirer.managerPrompt([
@@ -124,12 +128,32 @@ function internPrompt() {
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
+        // function render(){
+        //     {
+        //         inquirer.prompt(managerPrompt),
+        //         inquirer.prompt(engineerPrompt),
+        //         inquirer.prompt(internPrompt);
+        //     }
+            
+        //     .then(inputs => {
+        //         const response = Employee(inputs);
+        //         console.log(inputs);
+        //     })
+        //     .catch((err) =>{
+        //         console.log(err);
+        //     });
+        // };
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
+//Now write it to a file named `team.html` in thw `output` folder. 
+//You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
+        //render();
+
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. 
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
