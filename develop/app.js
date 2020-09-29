e6fe55502967580a43668356fc96aecc8e8b2afd
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-09-29 15:29:22 -0400

    reformatted prompt functions for basic questions that apply to all emps and a unique question function for the special case questions.

--- develop/app.js
+++ develop/app.js
@@ -17,25 +17,25 @@ const render = require("./lib/htmlRenderer");
 const employeesArray = [];
 
 
-console.log("Please build your team.")
+//console.log("Please build your team.")
 
 //Create a basic prompt function that takes in the same questions regardless of employee type.
-basicPrompt()
-function basicPrompt(userInput) {
-    inquirer.basicPrompt([
+basicEmployeePrompt()
+function basicEmployeePrompt(userInput) {
+    inquirer.prompt([
         {
             type: "input",
-            message: "Enter employee name.",
+            message: "Enter employee name?",
             name: "name" 
         },
         {
             type: "input",
-            message: "Please enter employee id.",
+            message: "Please enter employee id?",
             name: "id"
         },
         {
             type: "input",
-            message: "Now enter employee email.",
+            message: "Now enter employee email?",
             name: "email"
         },
         {
@@ -46,58 +46,74 @@ function basicPrompt(userInput) {
                 "Manager",
                 "Engineer",
                 "Intern",
-                "I don't want to add any more team members."
-            ]
+                ]
         },
     ]).then(function(res){
         uniquePrompt(res);
     }).catch(function(err){
         if(err) throw err;
-        console.log("basic prompt answers have been logged")
+        console.log("basic prompt answers have been logged.")
     })
 };
 
-//Define function for special case employee type questions
-function uniquePrompt() {
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
 
-    inquirer.uniquePrompt ([
-        {
-            type: "input",
-            message: "What is your engineer's name?",
-            name: "name" 
-        },
-        {
-            type: "input",
-            message: "What is your engineer's id?",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "What is your engineer's email?",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "What is your engineer's Github username?",
-            name: "github"
-        },
-        {
-            type: "list",
-            message: "Which type of team member would you like to add?",
-            name: "role",
-            choices: [
-                "Engineer",
-                "Intern",
-                "I don't want to add any more team members."
-            ]
-        },
-    ]);
+//Define function to ask for special case employee role-type questions
+
+function uniquePrompt(userReply) {
+    //conditional if employee is a manager
+    if(userReply.role === "Manager"){
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is your office number?",
+                name: "officeNumber"
+            }
+        ]).then(function(uniqueReply){
+            //create a manager variable to take in new manager data
+            const manager = new Manager(userReply.name, userReply.id, userReply.email, uniqueReply.officeNumber)
+            //push to employees array
+            employeesArray.push(manager);
+        }).catch(function(err){
+            if(err) throw err
+        })
+    //conditional for if employee is an engineer
+    } else if(userReply.role === "Engineer"){
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is your github username?",
+                name: "github"
+            }
+        ]).then(function(uniqueReply){
+            //create an engineer variable to take in new engineer data
+            const engineer = new Engineer(userReply.name, userReply.id, userReply.email, uniqueReply.github)
+            //push to employees array
+            employeesArray.push(engineer);
+        }).catch(function(err){
+            if(err) throw err
+        })
+    //conditional for if employee is an intern
+    } else {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What school did you attend?",
+                name: "school"
+            }
+        ]).then(function(uniqueReply){
+            //create an intern variable to take in new intern data
+            const intern = new Intern(userReply.name, userReply.id, userReply.email, uniqueReply.school)
+            //push to employees array
+            employeesArray.push(intern);
+        }).catch(function(err){
+            if(err) throw err
+        })
+    }
 };
-
-
-// After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
-
-render();
+// After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee
 
 //Now write it to a file named `team.html` in thw `output` folder. 
 //You can use the variable `outputPath` above target this location.
@@ -109,9 +125,7 @@ render();
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. 
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
@@ -120,8 +134,3 @@ render();
 // for the provided `render` function to work! ```
 
 
-// {
-//     type: "input",
-//     message: "What is your office number?",
-//     name: "officeNumber"
-// },
