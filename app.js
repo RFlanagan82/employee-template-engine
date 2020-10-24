26e9e5a9eda0cd1931da910fd2da3dd9e6231dfc
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-10-24 05:34:44 -0400

    fixed file paths on app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const Employee = require("./lib/Employee");
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Employee = require("./develop/lib/Employee");
+const Manager = require("./develop/lib/Manager");
+const Engineer = require("./develop/lib/Engineer");
+const Intern = require("./develop/lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "./develop/output");
+const outputPath = path.join(OUTPUT_DIR, ".team.html");
 
 //Need to create a variable that will be used as a function later that will dynamically build the HTML page
-const render = require("./lib/htmlRenderer");
+const render = require("./develop/lib/htmlRenderer");
 
 //Need to create an array containing all employee objects;
 const employeesArray = [];
